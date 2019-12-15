b1c8085d13ae382ed1a6dbdbd1ca8a102c6a3d1b
Max Lorenz <max-lorenz@gmx.net>
2019-12-15 20:33:04 +0800

    Refactor actions

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,13 @@ from actions.shout import ShoutAction
 from actions.hackernews import HNAction
 from actions.openweather import OpenWeatherAction
 
-ACTIONS = [ShoutAction, HNAction, OpenWeatherAction]
+ACTION_DICT = {
+    'shout': ShoutAction,
+    'hackernews': HNAction,
+    'openweather': OpenWeatherAction,
+}
+
+ACTIONS = list(ACTION_DICT.values())
 
 if __name__ == '__main__':
     for action in ACTIONS:
