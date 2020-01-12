758f6a201081e3c50ba8d5abf78134251f5e8fc3
fabianbormann <bormann.fabian@gmail.com>
2020-01-12 23:55:02 +0100

    emotion classifier is working closes #38

--- classifier/datasets/movie_review.py
+++ classifier/datasets/movie_review.py
@@ -16,45 +16,51 @@ from tqdm import tqdm
 import numpy as np
 
 class MovieReviewDataset(Dataset):
-    def __init__(self, path=os.path.dirname(os.path.abspath(__file__)), transform=None):
-        url = 'https://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz'
+    def __init__(self, path=os.path.dirname(os.path.abspath(__file__)), transform=None, restore_transformed_samples=None):
         self.base_path = os.path.join(path, 'movie_review_dataset')
         self.transform = transform
-        download_path = os.path.join(self.base_path, 'review_polarity.tar.gz')
+        if restore_transformed_samples:
+            with open(restore_transformed_samples, 'rb') as file:
+                self.samples = pickle.load(file)
+        else:
+            url = 'https://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz'
+            download_path = os.path.join(self.base_path, 'review_polarity.tar.gz')
 
-        if not os.path.isdir(self.base_path):
-            os.makedirs(self.base_path)
-            wget.download(url, download_path)
+            if not os.path.isdir(self.base_path):
+                os.makedirs(self.base_path)
+                wget.download(url, download_path)
 
-        if os.path.isfile(download_path):
-            with tarfile.open(download_path, 'r:gz') as tar:
-                tar.extractall(self.base_path)
-            os.unlink(download_path)
+            if os.path.isfile(download_path):
+                with tarfile.open(download_path, 'r:gz') as tar:
+                    tar.extractall(self.base_path)
+                os.unlink(download_path)
 
-        self.samples = []
-        print('Read negative samples')
-        for i, negative_sample in enumerate(tqdm(os.listdir(os.path.join(self.base_path, 'txt_sentoken', 'neg')))):
-            with open(os.path.join(self.base_path, 'txt_sentoken', 'neg', negative_sample)) as file:
+            self.samples = []
+            self.preprocess('positive')
+            self.preprocess('negative')
+
+    def store_transformed_samples(self, path):
+        with open(path, 'wb') as file:
+            pickle.dump(self.samples, file)
+
+    def preprocess(self, usage):
+        print('Read {} samples'.format(usage))
+        for i, sample in enumerate(tqdm(os.listdir(os.path.join(self.base_path, 'txt_sentoken', usage[:3])))):
+            with open(os.path.join(self.base_path, 'txt_sentoken', usage[:3], sample)) as file:
                 text = file.read()
                 text = text.replace('\n', '')
-                label = np.array(0)
+                label = np.array(usage == 'positive', dtype=np.int32)
                 if self.transform:
-                    text = self.transform[0](text)
-                    label = self.transform[1](label)
-
-                self.samples.append((text, label))
-
-        print('Read positive samples')
-        for i,positive_sample in enumerate(tqdm(os.listdir(os.path.join(self.base_path, 'txt_sentoken', 'pos')))):
-            with open(os.path.join(self.base_path, 'txt_sentoken', 'pos', positive_sample)) as file:
-                text = file.read()
-                text = text.replace('\n', '')
-                label = np.array(1)
-                if self.transform:
-                    text = self.transform[0](text)
-                    label = self.transform[1](label)
-
-                self.samples.append((text, label))
+                    sentences = text.split('.')
+                    data = []
+                    for sentence in sentences:
+                        data.append(self.transform[0](sentence))
+                      
+                    data = torch.mean(self.transform[1](data), 0).flatten()
+                    label = self.transform[2](label)
+                    self.samples.append((data, label, text))
+                else:
+                    self.samples.append((text, label))
 
     def __len__(self):
         return len(self.samples)
--- classifier/emotion.py
+++ classifier/emotion.py
@@ -8,65 +8,103 @@ from datasets.movie_review import MovieReviewDataset
 import math
 import torch
 from tqdm import tqdm
+import wget
+import os
 
-DEFAULT_MODEL = 'roberta-base-nli-stsb-mean-tokens'
+DEFAULT_MODEL = 'roberta-large-nli-stsb-mean-tokens'
 
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        self.dense1 = nn.Linear(768, 2048)
-        self.dense2 = nn.Linear(2048, 4096)
-        self.dense3 = nn.Linear(4096, 1)
-
+        self.linear1 = nn.Linear(1024, 2048)
+        self.linear2 = nn.Linear(2048, 2048)
+        self.linear3 = nn.Linear(2048, 512)
+        self.linear4 = nn.Linear(512, 1)
+        self.dropout = nn.Dropout(0.2)
+        
     def forward(self, x):
-        x = F.leaky_relu(self.dense1(x))
-        x = F.leaky_relu(self.dense2(x))
-        x = torch.sigmoid(self.dense3(x))
-        return x    
+        x = F.elu(self.linear1(x))
+        x = self.dropout(x)
+        x = F.elu(self.linear2(x))
+        x = self.dropout(x)
+        x = F.elu(self.linear3(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.linear4(x))
+        return x
 
 
 class EmotionClassifier:
     name = 'Emotion Classifier'
     description = "This classifier detects positive or negative sentiment in input texts"
 
-    def __init__(self, embedder=DEFAULT_MODEL):
-        print(f"Loading model {embedder}")
+    def __init__(self, embedder=DEFAULT_MODEL, restore_from_path=None, transformed_samples_path=None):
         self.embedder = SentenceTransformer(embedder)
         self.classifier = Classifier()
         self.criterion = nn.BCELoss()
-        self.optimizer = optim.SGD(self.classifier.parameters(), lr=0.001, momentum=0.9)
+        self.optimizer = optim.SGD(self.classifier.parameters(), lr=0.001)
+        self.transformed_samples_path = transformed_samples_path
 
-        dataset = MovieReviewDataset(transform=[
-            lambda x: torch.tensor(self.embedder.encode([x])),
-            lambda x: torch.tensor(x, dtype=torch.float)
-        ])
-        self.train_dataset, self.test_dataset = random_split(dataset, [math.ceil(len(dataset)*0.8), math.floor(len(dataset)*0.2)])
-        
-        self.train_dataloader = DataLoader(self.train_dataset, batch_size=32, shuffle=True, num_workers=2)
-        self.test_dataloader = DataLoader(self.test_dataset, batch_size=32, shuffle=True, num_workers=2)
+        if torch.cuda.is_available():
+            self.classifier.cuda()
 
-    def predict(self, sentence):
-        self.embedder.encode([sentence])
-        print('NOT IMPLEMENTED YET')
-        # use mlp (see train method)
+        if restore_from_path:
+            self.load(restore_from_path)
+
+    def predict(self, text):
+        text = text.replace('\n', '')
+        sentences = text.split('.')
+        data = []
+        for sentence in sentences:
+            data.append(self.embedder.encode([sentence]))
+            
+        data = torch.mean(torch.tensor(data), 0).flatten()
+
+        self.classifier.eval()
+        with torch.no_grad():
+            prediction = self.classifier(data)
+
+        return torch.round(prediction.squeeze()).item()
 
     def log(self, message, verbose):
         if verbose > 0:
             print(message)
 
+    def save(self, path):
+        torch.save(self.classifier.state_dict(), path)
+
+    def load(self, path):
+        if torch.cuda.is_available():
+            self.classifier.load_state_dict(torch.load(path))
+        else:
+            device = torch.device('cpu')
+            self.classifier.load_state_dict(torch.load(path, map_location=device))
+        self.classifier.eval()
+
     def train(self, epochs=10, output_per_step=None, verbose=0):
+        if not self.train_dataset:
+            dataset = MovieReviewDataset(transform=[
+                lambda x: embedder.encode([x]),
+                lambda x: torch.tensor(x),
+                lambda x: torch.tensor(x, dtype=torch.float)
+            ], restore_transformed_samples=self.transformed_samples_path)
+            self.train_dataset, self.test_dataset = random_split(dataset, [math.ceil(len(dataset)*0.8), math.floor(len(dataset)*0.2)])
+            
+            self.train_dataloader = DataLoader(self.train_dataset, batch_size=32, shuffle=True, num_workers=2)
+            self.test_dataloader = DataLoader(self.test_dataset, batch_size=32, shuffle=True, num_workers=2)
+        
         running_loss = 0
         self.log('Start training', verbose)
         for epoch in range(epochs):
             self.log('Start epoch {}'.format(epoch), verbose)
 
-            if verbose > 0:
-                batches = tqdm(self.train_dataloader)
-            else:
-                batches = self.train_dataloader
+            for i, batch in enumerate(self.train_dataloader):
+                self.classifier.train()
+                inputs, labels, _ = batch
 
-            for i, batch in enumerate(batches):
-                inputs, labels = batch
+                if torch.cuda.is_available():
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+
                 self.optimizer.zero_grad()
 
                 outputs = self.classifier(inputs)
@@ -76,13 +114,47 @@ class EmotionClassifier:
 
                 running_loss += loss.item()
                 if output_per_step:
-                    if i % 0 == output_per_step:
-                        print('[{}, {}] loss: {}'.format(epoch + 1, i + 1, running_loss / output_per_step))
+                    if i % output_per_step == output_per_step - 1:
+                        print('[Epoch {}] loss: {}'.format(epoch + 1, running_loss / output_per_step))
                         running_loss = 0.0
+                        correct_predictions = 0
+                        predictions = 0
+                        false_positives = 0
+                        false_negatives = 0
+                        true_positives = 0
+                        true_negatives = 0
+                        with torch.no_grad():
+                            self.classifier.eval()
+                            for test_batch in self.test_dataloader:
+                                test_inputs, test_labels, test_text = test_batch
 
+                                if torch.cuda.is_available():
+                                    test_inputs = test_inputs.cuda()
+                                    test_labels = test_labels.cuda()
 
+                                test_outputs = self.classifier(test_inputs)
+                                prediction = torch.round(test_outputs.squeeze())
+                                correct_predictions += torch.sum(prediction == test_labels).item()
+                                predictions += (prediction == test_labels).nelement()
+          
+                                false_positives += torch.sum((prediction - test_labels) == torch.ones_like(prediction)).item()
+                                false_negatives += torch.sum((prediction - test_labels) == (torch.ones_like(prediction) * -1)).item()
+                                true_positives += torch.sum((prediction + test_labels) == (torch.ones_like(prediction) * 2)).item()
+                                true_negatives += torch.sum((prediction + test_labels) == torch.zeros_like(prediction)).item()
+                        print('[Epoch {}] Accuracy: {}, FP: {} FN: {} TP: {} TN: {} Total: {}'.format(epoch + 1, 
+                            (correct_predictions/predictions) * 100, false_positives, false_negatives, true_positives, true_negatives, predictions))
+
+                                
 if __name__ == '__main__':
-    classifier = EmotionClassifier()
-    #sentence = input('Type a positve or negative sentence and press enter: ')
-    #classifier.predict(sentence)
-    classifier.train(verbose=1)
+    download_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'emotion_classifier.pt')
+    if not os.path.isfile(download_path):
+        wget.download('https://drive.google.com/file/d/1-wUMI9xNsODvSetQpJqZnH9oMMhno91G/view?usp=sharing', download_path)
+    
+    classifier = EmotionClassifier(restore_from_path=download_path)
+    text = input('Type a positve or negative text and press enter: ')
+    
+    is_positive = classifier.predict(text)
+    if is_positive:
+        print('Your text was classified as positive')
+    else:
+        print('Your text was classified as negative')
