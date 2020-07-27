6473359eb722bec43fc493ffb9e28861c5fb3bc9
fabianbormann <bormann.fabian@gmail.com>
2020-07-27 21:58:16 +0200

    repair BERT bot

--- botkit/actions/actions.py
+++ botkit/actions/actions.py
@@ -1,13 +1,19 @@
 """
 This namespace has the reference to all available actions
 """
-from actions.mail import SendMailAction
+from .mail import SendMailAction
+from .shout import ShoutAction
+from .hello import HelloAction
+from .hackernews import HNAction
 
 ACTIONS = [
+    ShoutAction,
     SendMailAction,
+    HelloAction,
+    HNAction
 ]
 
-ACTION_DICT = {a.name: a for a in ACTIONS}
+ACTION_DICT = { a.name: a for a in ACTIONS }
 
 if __name__ == '__main__':
     for action in ACTIONS:
