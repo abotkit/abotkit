2bb508e6a779944ab098255ac7a8b31b5e93e659
fabianbormann <bormann.fabian@gmail.com>
2020-10-23 18:01:18 +0200

    split abotkit and add submodules

--- ui/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Actions} from './Actions';
-export {default as Chat} from './Chat';
-export {default as Intents} from './Intents';
-export {default as Settings} from './Settings';
-export {default as About} from './About';
-export {default as BotNotFound} from './BotNotFound'; 
